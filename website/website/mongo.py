6037d40ffe39c978078356f1e3b9c7d421d29fdb
tony_han <tonyhan@gmail.com>
2024-03-02 15:23:14 -0800

    add dashboard page

--- website/website/mongo.py
+++ website/website/mongo.py
@@ -1,11 +1,42 @@
 import pymongo
+import json
 from pymongo import MongoClient
+import os
+
 
 cluster = MongoClient("mongodb+srv://Viewer:1234@sensordata.1v3p4ie.mongodb.net/?retryWrites=true&w=majority")
+# cluster = MongoClient("mongodb+srv://tonyhan:tonyhan@sensordata.1v3p4ie.mongodb.net/?retryWrites=true&w=majority")
 db = cluster["test"]  # space in cloud
 
-collection = db["test"]  # table
+collection = db["data"]  # table
 
+current_directory = os.getcwd()
+print(current_directory)
+
+# Specify the filename
+json_filename = 'data.json'
+
+# Create the full path to the JSON file
+json_file_path = os.path.join(current_directory, json_filename)
+
+# Open the JSON file
+# with open(json_file_path, 'r') as file:
+#     json_data = json.load(file)
+
+# print(json_data)
+# collection.insert_one(json_data)
+
+# print(result)
+# cursor = collection.find({})
+
+
+# Print the data
+# print(cursor)
+# for document in cursor:
+#     print(document)
+
+# Close the MongoDB connection
+# cluster.close()
 
 #post = {"_id": 0, "temp": 70, "pressure": 2, "humidity" : 16}
 
--- website/website/views.py
+++ website/website/views.py
@@ -1,14 +1,31 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, Flask, jsonify, render_template
 
 from website.mongo import db
 
-views = Blueprint('views', __name__)
+views = Blueprint('views', __name__, static_folder='static')
+# views = Flask('views', __name__,  static_folder='static/')
 
 @views.route('/')
 def home():
 
-    data = db.test.find({})
+    data = list(db.test.find({}))
     for result in data:
         print(result)
 
     return render_template('home.html', data = data)
+
+
+@views.route('data_table/')
+def table():
+    # sensor_data = {}
+    data = list(db.test.find({}))  # Convert cursor to list
+    print(data)
+    for result in data:
+        print(result)
+
+    return render_template('data_table.html', data=data)
+
+@views.route('dashboard/')
+def dashboard():
+
+    return render_template('index.html', title="Eco_Health_Tracker")
