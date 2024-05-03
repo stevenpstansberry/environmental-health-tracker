e74ae70dcf52f87d68722ff69efe38beab75d16d
maryiasakharava <99642282+maryiasakharava@users.noreply.github.com>
2024-05-03 14:24:27 -0700

    Update upload.py
    attempt 2 uploading to the database

--- sensors/upload.py
+++ sensors/upload.py
@@ -1,29 +1,35 @@
 from pymongo import MongoClient
 import json
+from pymongo.errors import ServerSelectionTimeoutError
 
+def upload_json_to_mongo(json_file, uri):
+    try:
+        # Establish a connection to the MongoDB server
+        client = MongoClient(uri, serverSelectionTimeoutMS=5000)  # Adjust timeout as needed
 
-# this should take in json file as sensor_data.json
-def upload_json_to_mongo(json_file, uri='mongodb+srv://maryiasakharava:SensorDataPassword@sensordata.pjdhs9k.mongodb.net/?retryWrites=true&w=majority&appName=SensorData'):
-    # Establish a connection to the MongoDB server
-    client = MongoClient(uri)
+        # Select the database and collection
+        db = client['EcoHealthTracking']
+        collection = db['SensorData']
 
-    # Select the database and collection
-    db = client['FloraHealthTracker']
-    collection = db['SensorData']
+        # Read the JSON file
+        with open(json_file, 'r') as file:
+            data = json.load(file)
 
-    # Read the JSON file
-    with open(json_file, 'r') as file:
-        data = json.load(file)
+        # Upload the data to MongoDB
+        for sensor_type, readings in data.items():
+            for reading in readings:
+                # Ensure each reading is a document and has a sensor type
+                reading['sensor'] = sensor_type
+                collection.insert_one(reading)
 
-    # Upload the data to MongoDB
-    for sensor_type, readings in data.items():
-        for reading in readings:
-            # Ensure each reading is a document and has a sensor type
-            reading['sensor'] = sensor_type
-            collection.insert_one(reading)
+        print("Data uploaded successfully!")
 
-    # Close the connection
-    client.close()
+    except ServerSelectionTimeoutError:
+        print("Server selection timeout error: Unable to connect to the MongoDB server.")
+
+    finally:
+        # Close the connection
+        client.close()
 
 # Call the function to upload the data
 upload_json_to_mongo('sensor_data.json', uri='mongodb+srv://maryiasakharava:SensorDataPassword@sensordata.pjdhs9k.mongodb.net/?retryWrites=true&w=majority&appName=SensorData')
